45f340e72095873ec46f07c4271856d9fa230d2e
Bruno Bouwman <brunobouwman@hotmail.com>
2022-03-02 13:09:42 +0000

    just missing the favorites page/functionalities

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupsPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
+
 function App() {
   return (
   <div>
-    <MainNavigation />
+    <Layout>
     <Switch> {/*By default Switch 'stops' whenever theres a common path among all routes (in this case '/') */}
       <Route path="/" exact>  {/*Initial page. 'exact checks the full path*/}
         <AllMeetupsPage />
@@ -19,6 +20,7 @@ function App() {
         <FavoritesPage />
       </Route>
     </Switch>
+    </Layout>
   </div>
   );
 }
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,31 @@
-function NewMeetupsPage() {
-    return <div>New meetup Page</div>
-};
+import { useHistory } from 'react-router-dom';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
+function NewMeetupsPage() {
+    const history = useHistory(); // the history method/property helps us with the navigation between 'pages'
+    
+
+    function addMeetupHandler(meetupData) {
+          fetch(
+    'https://react-meetupapp-d7408-default-rtdb.firebaseio.com/meetups.json',
+    {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+  ).then(() => {
+      history.replace('/'); //Navigates us awaybut won't allow the use of the back button from the browser
+  })
+}
+
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
 export default NewMeetupsPage;
